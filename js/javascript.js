1190fcf69c7ca7221550d94cbede4599d3a6ec42
Jeremias Arrieta <kekardo@hotmail.com>
2022-07-18 19:23:35 -0300

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,14 @@
+// let ingresarNumero = parseInt(prompt("Ingresar Numero"));
+// for (let i = 1; i <= 10; i++) {
+//     let resultado = ingresarNumero * i ;
+//     alert(ingresarNumero +" X "+ i +" = "+ resultado);
+//     console.log("Estas multiplicando a tu numero por " + i);
+// } 
+
+
+// let nombre = prompt("Ingresar un dato");
+// while (nombre != "Jeremias") {
+//     alert("El usuario ingresó " + nombre);
+//     nombre = prompt("Ingresar otro dato");
+//     console.log("Su nombre ingresado es " + nombre);
+// }
